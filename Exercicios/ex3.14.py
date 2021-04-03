4019590976f284c77cc5c768f4958354a9c51662
MateusTheodoro <mateus-jau@hotmail.com>
2021-04-02 23:46:07 -0300

    Update

--- Exercicios/ex3.14.py
+++ Exercicios/ex3.14.py
@@ -1,5 +1,5 @@
 km = int(input('Digite a quantidade em km: '))
 dias = int(input('Digite a quantidade de dias: '))
-preco = (60 * dias)
+preco = (60 * dias) + (km * 0.15)
 
-print(f'{preco}')
+print(f'O preço será de R${preco:.2f}')
---
+++ Exercicios/ex3.15.py
@@ -0,0 +1,8 @@
+cigarros = int(input('Digite a quantide de cigarros por dia: '))
+anos = int(input('Quantos anos você fuma? '))
+
+dias = anos * 365
+minutos = cigarros * 10
+
+print(f'--- Considerando que você perde 10 minutos da vida fumando ---')
+print(f'Você já fumou {dias} dias')
