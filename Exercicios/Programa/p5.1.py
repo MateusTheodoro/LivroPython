0ec11f5b6a72f55ee50c84c2deb9f026d805a487
MateusTheodoro <61423297+MateusTheodoro@users.noreply.github.com>
2021-07-09 20:25:04 -0300

    update

--- Exercicios/Programa/p5.1.py
+++ Exercicios/Programa/p5.1.py
@@ -1,7 +1,14 @@
-foguete = int(input('Você vai lançar o foguete. Digite a contagem regressiva: '))
-x = 1
-while x <= foguete:
-    print(foguete)
-    foguete = foguete - 1
+# foguete = int(input('Você vai lançar o foguete. Digite a contagem regressiva: '))
+# x = 1
+# while x <= foguete:
+#     print(foguete)
+#     foguete = foguete - 1
+#
+# print('FOGO! ')
 
-print('FOGO! ')
+fim = int(input('Digite o último Nro: '))
+x = 1
+while x <= fim:
+    print(x)
+    x = x + 1
+
