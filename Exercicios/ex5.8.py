dd576631e37520b5e911b6e99037e537c4cb3979
MateusTheodoro <61423297+MateusTheodoro@users.noreply.github.com>
2021-07-10 13:43:50 -0300

    Create ex5.8.py

---
+++ Exercicios/ex5.8.py
@@ -0,0 +1,13 @@
+"""
+Escreva um programa que leia dois números.
+Imprima o resultado da multiplicação do primeiro pelo segundo.
+Utilize apenas os operadores de soma e subtração para calcular o resultado.
+Lembre-se de que podemos entender a multiplicação de dois números como somas sucessivas de um deles.
+Assim, 4x5 = 5+5+5+5 = 4+4+4+4+4
+"""
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+
+
+while n1 < n2:
+
