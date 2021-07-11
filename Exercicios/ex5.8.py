427fd73c5f21c7b3b7b162bec3098baea8caaeff
MateusTheodoro <61423297+MateusTheodoro@users.noreply.github.com>
2021-07-11 14:10:00 -0300

    Update ex5.8.py

--- Exercicios/ex5.8.py
+++ Exercicios/ex5.8.py
@@ -7,3 +7,20 @@ Assim, 4x5 = 5+5+5+5 = 4+4+4+4+4
 """
 n1 = int(input('Digite um número: '))
 n2 = int(input('Digite outro número: '))
+
+x = 1
+while x <= n2:
+    print(x)
+    x = x + 1
+
+
+
+#
+# x = 1
+# while x <= 10:
+#     y = 1
+#     while y <= 5:
+#         print(f'{x}, {y}')
+#         y += 1
+#     x += 1
+# print('Acabou!')
