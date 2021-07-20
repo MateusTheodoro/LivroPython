b987d0b2a7982fedf8feddfb0ddd7dc7d7a08b0e
MateusTheodoro <61423297+MateusTheodoro@users.noreply.github.com>
2021-07-20 18:05:15 -0300

    up

--- Arquivos/Cap. 4/ex4.1.py
+++
@@ -1,7 +0,0 @@
-n1 = int(input('Digite um número: '))
-n2 = int(input('Digite outro número: '))
-
-if n1 > n2:
-    print('O primeiro valor é maior')
-if n2 > n1:
-    print('O segundo valor é maior')
--- Arquivos/Cap. 4/ex4.5.py
+++
@@ -1,6 +0,0 @@
-idade = int(input('Qual a idade do seu carro? '))
-
-if idade <= 3:
-    print('Seu carro é novo!')
-else:
-    print('Seu carro já está antigo')
