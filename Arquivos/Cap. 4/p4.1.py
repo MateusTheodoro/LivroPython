3040215f1b22101a5d646c92cd1670f9ce7a6e06
MateusTheodoro <61423297+MateusTheodoro@users.noreply.github.com>
2021-09-22 19:52:10 -0300

    pastas organizadas

---
+++ Arquivos/Cap. 4/p4.1.py
@@ -0,0 +1,7 @@
+# Programa 4.1 - Lê dois valores e imprime qual é o maior
+a = int(input("Primeiro valor: "))
+b = int(input("Segundo valor:"))
+if a > b:
+    print("O primeiro valor é maior!")
+if b > a:
+    print("O segundo valor é maior!")
