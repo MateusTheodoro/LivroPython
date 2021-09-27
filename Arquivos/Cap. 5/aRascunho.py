1697b8a1eb4d0a4222cc63fc1b6af42423407dcd
MateusTheodoro <61423297+MateusTheodoro@users.noreply.github.com>
2021-09-26 21:03:31 -0300

    Create aRascunho.py

---
+++ Arquivos/Cap. 5/aRascunho.py
@@ -0,0 +1,16 @@
+# x = 1
+# while x <= 3:
+#     x = x + 1
+#     print(x)
+
+# x = 10
+# while x >= 0:
+#     print(x)
+#     x = x - 1
+# print("Fogo!!")
+
+fim = int(input('Último número: '))
+x = 1
+while x <= fim:
+    print(x)
+    x += 1
