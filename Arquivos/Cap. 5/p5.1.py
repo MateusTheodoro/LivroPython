c84e4c652c09f8c9647d6417501afae109486131
MateusTheodoro <61423297+MateusTheodoro@users.noreply.github.com>
2021-10-30 20:10:25 -0300

    Update p5.1.py

--- Arquivos/Cap. 5/p5.1.py
+++ Arquivos/Cap. 5/p5.1.py
@@ -1,14 +1,23 @@
-# foguete = int(input('Você vai lançar o foguete. Digite a contagem regressiva: '))
-# x = 1
-# while x <= foguete:
-#     print(foguete)
-#     foguete = foguete - 1
-#
-# print('FOGO! ')
+# contagem de cédulas
 
-fim = int(input('Digite o último Nro: '))
-x = 1
-while x <= fim:
-    print(x)
-    x = x + 1
-
+valor = int(input("Digite o valor a pagar: "))
+cedulas = 0
+atual = 50
+apagar = valor
+while True:
+    if atual <= apagar:
+        apagar -= atual
+        cedulas += 1
+    else:
+        print(f"{cedulas} cédulas(s) de R${atual}")
+        if apagar == 0:
+            break
+        if atual == 50:
+            atual = 20
+        elif atual == 20:
+            atual = 10
+        elif atual == 10:
+            atual = 5
+        elif atual == 5:
+            atual = 1
+        cedulas = 0
