fb6340f66b00c230dda463ed8327b45cf26698b0
MateusTheodoro <61423297+MateusTheodoro@users.noreply.github.com>
2021-11-09 17:27:42 -0300

    Update aRascunho.py

--- Arquivos/Cap. 5/aRascunho.py
+++ Arquivos/Cap. 5/aRascunho.py
@@ -16,10 +16,18 @@
 #     x += 1
 #
 
-s = 0
-while True:
-    v = int(input("Digite um nro a somar ou 0 p/ sair: "))
-    if v == 0:
-        break
-    s += v
-print(s)
+# s = 0
+# while True:
+#     v = int(input("Digite um nro a somar ou 0 p/ sair: "))
+#     if v == 0:
+#         break
+#     s += v
+# print(s)
+
+tabuada = 1
+while tabuada <= 10:
+    numero = 1
+    while numero <= 10:
+        print(f"{tabuada} x {numero} = {tabuada * numero}")
+        numero += 1
+    tabuada += 1
