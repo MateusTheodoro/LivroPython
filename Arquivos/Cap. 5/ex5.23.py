88ccf6e3f2b56f46e3ee32f91e3bf9498bb65636
MateusTheodoro <61423297+MateusTheodoro@users.noreply.github.com>
2021-11-15 15:26:06 -0300

    Update ex5.23.py

--- Arquivos/Cap. 5/ex5.23.py
+++ Arquivos/Cap. 5/ex5.23.py
@@ -5,5 +5,23 @@ por 2 e depois por todos os números ímpares até o número lido.
 Se o resto de uma dessas divisões for igual a zero, o número não é primo.
 Observe que 0 e 1 não são primos e que 2 é o único número que é par.
 """
-numero = int (input("Digite um número: "))
-
+nro = int(input("Digite um número: "))
+if nro < 0:
+    print("Número inválido. Digite apenas valores positivos")
+if nro == 0 or nro == 1:
+    print(f"{nro} é um caso especial.")
+else:
+    if nro == 2:
+        print("2 é primo")
+    elif nro % 2 == 0:
+        print(f"{nro} não é primo, pois 2 é o único número par primo")
+    else:
+        x = 3
+        while(x < nro):
+            if nro % x == 0:
+                break
+            x += 2
+        if x == nro:
+            print(f"{nro} é primo")
+        else:
+            print(f"{nro} não é primo, pois é divisível por {x}")
